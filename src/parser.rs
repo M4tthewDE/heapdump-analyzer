e0ac3b6f3813e91e14ab40a5e273e4c782895852
M4tthewDE <matthias.kronberg@gmail.com>
2025-10-04 11:07:08 +0200

    refactor: use anyhow for error handling

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,4 @@
+use anyhow::{Result, anyhow};
 use std::{fs::File, io::Read, path::Path};
 
 #[derive(Debug)]
@@ -6,28 +7,29 @@ pub enum Version {
 }
 
 impl Version {
-    fn new(version_str: &str) -> Version {
+    fn new(version_str: &str) -> Result<Version> {
         match version_str {
-            "JAVA PROFILE 1.0.2" => Version::JavaProfile102,
-            _ => todo!(),
+            "JAVA PROFILE 1.0.2" => Ok(Version::JavaProfile102),
+            _ => Err(anyhow!("Invalid version: {}", version_str)),
         }
     }
 }
 
+// https://github.com/openjdk/jdk17/blob/4afbcaf55383ec2f5da53282a1547bac3d099e9d/src/hotspot/share/services/heapDumper.cpp#L62
 #[derive(Debug)]
 pub struct Heap {
     pub version: Version,
 }
 
-pub fn parse(path: &Path) -> Heap {
-    let mut file = File::open(path).unwrap();
+pub fn parse(path: &Path) -> Result<Heap> {
+    let mut file = File::open(path)?;
 
     let mut version_buf = [0; 18];
-    file.read_exact(&mut version_buf).unwrap();
+    file.read_exact(&mut version_buf)?;
 
-    let version_str = String::from_utf8(version_buf.to_vec()).unwrap();
+    let version_str = String::from_utf8(version_buf.to_vec())?;
 
-    Heap {
-        version: Version::new(&version_str),
-    }
+    Ok(Heap {
+        version: Version::new(&version_str)?,
+    })
 }
