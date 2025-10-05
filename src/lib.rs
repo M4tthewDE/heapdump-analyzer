d7762d9b382b0108e8a9e24853218e40840240f4
M4tthewDE <matthias.kronberg@gmail.com>
2025-10-05 12:31:16 +0200

    feat: parse class dump sub record

--- src/lib.rs
+++ src/lib.rs
@@ -88,6 +88,10 @@ pub enum Record {
         class_serial_number: u32,
         line_number: i32,
     },
+    HeapDumpSegment {
+        micros: u32,
+        sub_records: Vec<SubRecord>,
+    },
     HeapDumpEnd,
 }
 
@@ -98,11 +102,12 @@ impl Record {
         let bytes_remaining = read_u32(file)? as usize;
 
         match tag {
-            1 => Ok(Self::utf8(file, micros, bytes_remaining)?),
-            2 => Ok(Self::load_class(file, micros)?),
-            4 => Ok(Self::frame(file, micros)?),
-            5 => Ok(Self::trace(file, micros)?),
-            _ => Err(anyhow!("invalid tag: {}", tag)),
+            0x01 => Ok(Self::utf8(file, micros, bytes_remaining)?),
+            0x02 => Ok(Self::load_class(file, micros)?),
+            0x04 => Ok(Self::frame(file, micros)?),
+            0x05 => Ok(Self::trace(file, micros)?),
+            0x1c => Ok(Self::heap_dump_segment(file, micros)?),
+            _ => Err(anyhow!("invalid tag: 0x{:x}", tag)),
         }
     }
 
@@ -162,6 +167,133 @@ impl Record {
             line_number,
         })
     }
+
+    fn heap_dump_segment(file: &mut File, micros: u32) -> Result<Self> {
+        let mut sub_records = Vec::new();
+        loop {
+            let sub_record = SubRecord::new(file)?;
+            if matches!(sub_record, SubRecord::HeapDumpEnd) {
+                sub_records.push(sub_record);
+                break;
+            }
+            sub_records.push(sub_record);
+        }
+
+        Ok(Self::HeapDumpSegment {
+            micros,
+            sub_records,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum FieldValue {
+    NormalObject { object_id: u64 },
+}
+
+#[derive(Debug)]
+pub struct Field {
+    pub name_id: u64,
+    pub value: FieldValue,
+}
+
+impl Field {
+    fn new(file: &mut File) -> Result<Self> {
+        let name_id = read_u64(file)?;
+        let typ = read_u8(file)?;
+
+        let value = match typ {
+            0x02 => FieldValue::NormalObject {
+                object_id: read_u64(file)?,
+            },
+            _ => bail!("invalid field type: 0x{:x}", typ),
+        };
+
+        Ok(Self { name_id, value })
+    }
+}
+
+#[derive(Debug)]
+pub struct FieldDescriptor {
+    pub name_id: u64,
+    pub typ: u8,
+}
+
+#[derive(Debug)]
+pub enum SubRecord {
+    ClassDump {
+        class_object_id: u64,
+        stack_trace_serial_number: u32,
+        super_class_object_id: u64,
+        class_loader_object_id: u64,
+        signers_object_id: u64,
+        protection_domain_object_id: u64,
+        reserved1: u64,
+        reserved2: u64,
+        instance_size: u32,
+        constant_pool_size: u16,
+        number_of_static_fields: u16,
+        static_fields: Vec<Field>,
+        number_of_instance_fields: u16,
+        instance_field_descriptors: Vec<FieldDescriptor>,
+    },
+    HeapDumpEnd,
+}
+
+impl SubRecord {
+    pub fn new(file: &mut File) -> Result<Self> {
+        let sub_record_type = read_u8(file)?;
+
+        match sub_record_type {
+            0x20 => Self::class_dump(file),
+            _ => bail!("unknown sub record type: 0x{:x}", sub_record_type),
+        }
+    }
+
+    fn class_dump(file: &mut File) -> Result<Self> {
+        let class_object_id = read_u64(file)?;
+        let stack_trace_serial_number = read_u32(file)?;
+        let super_class_object_id = read_u64(file)?;
+        let class_loader_object_id = read_u64(file)?;
+        let signers_object_id = read_u64(file)?;
+        let protection_domain_object_id = read_u64(file)?;
+        let reserved1 = read_u64(file)?;
+        let reserved2 = read_u64(file)?;
+        let instance_size = read_u32(file)?;
+        let constant_pool_size = read_u16(file)?;
+
+        let number_of_static_fields = read_u16(file)?;
+        let mut static_fields = Vec::new();
+        for _ in 0..number_of_static_fields {
+            static_fields.push(Field::new(file)?);
+        }
+
+        let number_of_instance_fields = read_u16(file)?;
+        let mut instance_field_descriptors = Vec::new();
+        for _ in 0..number_of_instance_fields {
+            instance_field_descriptors.push(FieldDescriptor {
+                name_id: read_u64(file)?,
+                typ: read_u8(file)?,
+            });
+        }
+
+        Ok(Self::ClassDump {
+            class_object_id,
+            stack_trace_serial_number,
+            super_class_object_id,
+            class_loader_object_id,
+            signers_object_id,
+            protection_domain_object_id,
+            reserved1,
+            reserved2,
+            instance_size,
+            constant_pool_size,
+            number_of_static_fields,
+            static_fields,
+            number_of_instance_fields,
+            instance_field_descriptors,
+        })
+    }
 }
 
 fn read_utf8(r: &mut impl Read, size: usize) -> Result<String> {
@@ -200,6 +332,12 @@ fn read_u8(r: &mut impl Read) -> Result<u8> {
     let mut buf = [0; 1];
     r.read_exact(&mut buf)?;
     Ok(buf[0])
+}
+
+fn read_u16(r: &mut impl Read) -> Result<u16> {
+    let mut buf = [0; 2];
+    r.read_exact(&mut buf)?;
+    Ok(u16::from_be_bytes(buf))
 }
 
 fn read_u32(r: &mut impl Read) -> Result<u32> {
